477ad9d3aac481903c54b8b1d666717e5745409a
aroxan9999999 <112550824+aroxan9999999@users.noreply.github.com>
2025-02-20 18:17:22 +0400

    Update settings.py
    update function name

--- src/core/settings.py
+++ src/core/settings.py
@@ -189,7 +189,7 @@ if SENTRY_SETTINGS.get("dsn") and not DEBUG:
 
 CELERY_BEAT_SCHEDULE = {
     'cleanup-outboxes-every-60-minutes': {
-        'task': 'users.tasks.process_expired_outboxes',
+        'task': 'users.tasks.process_outbox',
         'schedule': 60 * 4,
     },
 }
